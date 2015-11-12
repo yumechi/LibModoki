bd9fd95ce0b20f2fd1cf223e6ed145e3588fdc55
yumechi <yumechi@users.noreply.github.com>
2015-11-13 02:09:49 +0900

    GCD and LCM Python ver

---
+++ Python/PythonGCD.py
@@ -0,0 +1,30 @@
+def gcd(a, b):
+    if a < b:
+        a,b = b,a
+    while b > 0:
+        r = a % b
+        a,b = b,r
+    return a
+
+def arrgcd(a):
+    ret = a[0]
+    for i in a[1:]:
+        ret = gcd(ret, i)
+    return ret
+
+def lcm(a, b):
+    if a == 0 or b == 0:
+        print("Error")
+        return 0
+    return int(a * b / gcd(a, b))
+
+def arrlcm(a):
+    ret = a[0]
+    for i in a[1:]:
+        ret = lcm(ret, i)
+    return ret
+
+print(arrgcd([3, 6, 9]))
+print(arrgcd([5, 8, 10]))
+print(arrlcm([3, 6, 9]))
+print(arrlcm([5, 8, 10]))
